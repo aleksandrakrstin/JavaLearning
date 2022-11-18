d3b7ff8f82109d0419c9373359dddd6abe1da8ee
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 10:29:56 +0100

    5th commit

--- src/Variables/Zadatak4.java
+++ src/Variables/Zadatak4.java
@@ -4,10 +4,14 @@ public class Zadatak4 {
     public static void main(String[] args) {
         int a = 10;
         int b = 2;
+        int sum = (a + b);
+        int difference = (a - b);
+        int division = (a / b);
+        int product = (a * b);
 
-        System.out.println(a + b);
-        System.out.println(a - b);
-        System.out.println(a / b);
-        System.out.println(a * b);
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(division);
+        System.out.println(product);
     }
 }
---
+++ src/Variables/Zadatak5.java
@@ -0,0 +1,8 @@
+package Variables;
+
+public class Zadatak5 {
+    public static void main(String[] args) {
+
+    }
+
+}
