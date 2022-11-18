7ee97b4fb1706cd4f61367cbe910c3540bb13674
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 09:36:16 +0100

    second commit

---
+++ src/Variables/Zadatak1.java
@@ -0,0 +1,11 @@
+package Variables;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        int y = 25;
+        System.out.println(y);
+    }
+}
