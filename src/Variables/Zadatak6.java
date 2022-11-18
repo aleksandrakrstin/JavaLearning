db52b09cee069ee8d471ee751c4c9dea4185ea8a
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 10:42:11 +0100

    7th commit

---
+++ src/Variables/Zadatak6.java
@@ -0,0 +1,10 @@
+package Variables;
+
+public class Zadatak6 {
+    public static void main(String[] args) {
+        int a = 10;
+        int sum1 = a + 10;
+
+        System.out.println(sum1);
+    }
+}
