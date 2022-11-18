4df1d2eac1b4652625987a10c4b4871fb783d9b1
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 10:16:27 +0100

    5th commit

---
+++ src/Variables/Zadatak4.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class Zadatak4 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+    }
+}
