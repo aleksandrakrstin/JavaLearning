bb999c125fe6be1c667b84bd8bb7a3ae7040e5f0
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 10:39:16 +0100

    6th commit

--- src/Variables/Zadatak5.java
+++ src/Variables/Zadatak5.java
@@ -2,7 +2,13 @@ package Variables;
 
 public class Zadatak5 {
     public static void main(String[] args) {
+        int a = 24;
+        int surface = a * a;
+        System.out.println(surface);
 
+        int r = 5;
+        double P = r * r * 3.14;
+        System.out.println(P);
     }
 
 }
