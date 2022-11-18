8700d84593a419431e5efc07b37f80fd7a874a3f
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 09:49:26 +0100

    third commit

---
+++ src/Variables/Zadatak2.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class Zadatak2 {
+    public static void main(String[] args) {
+        String ime = "Petar";
+        System.out.println("ime: " + ime);
+
+        String prezime = "Petrovic";
+        System.out.println("prezime: " + prezime);
+
+        String email = "ppetrovic@gmail.com";
+        System.out.println("email: " + email);
+    }
+
+}
