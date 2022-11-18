c802a7ce8f0d3a512f65ca51d73b334224577184
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-18 11:48:28 +0100

    8th commit

---
+++ src/Inputs/Zadatak1.java
@@ -0,0 +1,24 @@
+package Inputs;
+
+import java.util.Scanner;
+
+public class Zadatak1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+
+        int a = scan.nextInt();
+
+        System.out.println("Enter another number");
+
+        int b = scan.nextInt();
+
+        int sum = a + b;
+
+        double m = (sum / 2);
+
+        System.out.println("Mean = " + m);
+    }
+}
