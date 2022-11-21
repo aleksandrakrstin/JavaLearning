599a5c3625f42de2c868f5d98508d13f25f920b4
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-21 17:30:19 +0100

    13th commit

---
+++ src/IfElse/Zadatak8.java
@@ -0,0 +1,22 @@
+package IfElse;
+
+public class Zadatak8 {
+    public static void main(String[] args) {
+
+        int x = 99;
+        int y = 23;
+        int z = 10;
+
+       if (x < y && y < z && x < z) {
+           System.out.println("Values are in an increasing order.");
+       }
+       else if (x > y &&  y > z && x > z) {
+           System.out.println("Values are in a decreasing order.");
+       }
+       else {
+           System.out.println("Values not sorted.");
+       }
+    }
+
+
+}
