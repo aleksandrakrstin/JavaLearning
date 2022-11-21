f56d2c3df71438f4d37eaec7799e047dde75da5b
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-21 13:53:24 +0100

    9th commit

---
+++ src/Inputs/Zadatak3.java
@@ -0,0 +1,18 @@
+package Inputs;
+
+import java.util.Scanner;
+
+public class Zadatak3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a whole number of your choice:");
+        int a = scan.nextInt();
+        int quote = 0;
+
+        if (a > quote){
+            System.out.println("Broj je veci od " + quote);
+        }
+
+    }
+}
