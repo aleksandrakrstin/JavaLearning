1885def20633f86496390c5a7fc2c6788d5eb302
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-21 16:51:58 +0100

    12th commit

---
+++ src/IfElse/Zadatak7.java
@@ -0,0 +1,14 @@
+package IfElse;
+
+public class Zadatak7 {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 23;
+        int z = 99;
+
+        if (x < y && y < z && x < z) {
+            System.out.println("Values are in an increasing order.");
+        }
+    }
+}
