f2e6d794d3efb6008e07b3c88cca4ff8912a234e
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-22 09:32:22 +0100

    Zadatak 8. If / Else, increasing / decreasing order.

--- src/IfElse/Zadatak8.java
+++ src/IfElse/Zadatak8.java
@@ -3,9 +3,9 @@ package IfElse;
 public class Zadatak8 {
     public static void main(String[] args) {
 
-        int x = 99;
+        int x = 10;
         int y = 23;
-        int z = 10;
+        int z = 99;
 
        if (x < y && y < z && x < z) {
            System.out.println("Values are in an increasing order.");
