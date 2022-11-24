adb4720f40c20ef6a35aa4ecb28d0d3afb9dc141
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-11-24 15:31:37 +0100

    Task 1. Arrays.

---
+++ src/Arrays/Task1.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        String[] day = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        if (day[0].equals("Monday")) {
+            System.out.println(day[2]);
+        }
+    }
+}
