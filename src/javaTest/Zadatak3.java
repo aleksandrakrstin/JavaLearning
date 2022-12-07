ea153ce95e0534c280241c38e189cc2364a1ca96
aleksandrakrstin <aleksandra.krstin@vegait.rs>
2022-12-07 10:11:43 +0100

    Zadatak 3. Add numbers until user enters a 0 using a Loop.

--- src/javaTest/Zadatak3.java
+++ src/javaTest/Zadatak3.java
@@ -1,4 +1,22 @@
 package javaTest;
 
+import java.util.Scanner;
+
 public class Zadatak3 {
-}
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int i = 1;
+    int sum = 0;
+    while (i > 0) {
+      System.out.println("Please enter " + i + ". number:");
+      int num1 = scan.nextInt();
+        if (num1 == 0) {
+        break;
+      }
+      sum = sum + num1;
+      System.out.println("The sum is " + sum );
+      i++;
+      }
+    }
+  }
